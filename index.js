638e87d0d5581c691a6baf203da95d71de1b5cca
Xavier Inuca <bryan_xavi25@hotmail.com>
2017-11-13 18:42:09 -0500

    Cambio Index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express =  require ( ' express ' )
+var app =  express ()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+aplicación . set ( ' puerto ' , ( proceso . env . PORT  ||  5000 ))
+aplicación . use ( express . static ( __dirname  +  ' / public ' ))
 
-app.get('/', function(request, response) {
-  response.send(‘Prueba de OPTATIVA II, XAVIER INUCA, CISIC !')
+aplicación . get ( ' / ' , función ( solicitud , respuesta ) {
+  respuesta . enviar ( ' ¡Hola mundo! ' )
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+aplicación . listen ( app . get ( ' puerto ' ), función () {
+  consola . log ( "La aplicación Node se está ejecutando en localhost: "  +  app . get ( ' port ' ))
 })
